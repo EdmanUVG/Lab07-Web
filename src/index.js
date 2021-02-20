b422e6f85be1e90ca21537bc319e11a98a6ee52b
edman <edman@LAPTOP-IUQHGQ9D>
2021-02-20 17:14:36 -0600

    Add first loaders to handle CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+
 import { run } from "./app/app";
+import "./css/style.css"
 import { Stars } from "./app/stars";
 import { IntroStory } from "./app/intro_story";
+
 
 const stars = new Stars();
 const introStory = new IntroStory();
